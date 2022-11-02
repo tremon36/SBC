05abf2ba36a11652b7801f14c1a5f07d084fdf1a
Richi <80686731+tremon36@users.noreply.github.com>
2022-11-02 09:48:42 +0100

    bug fix

--- tof.h
+++ tof.h
@@ -3,7 +3,7 @@
 
 void tof_init(int scl_pin,int sda_pin,int irq_pin);
 void tof_init_interrupts(void(*isr_handler)(void*),void* args);
-void tof_request_read();
+void tof_request_measurement();
 float tof_get_last_measurement();
 uint8_t tof_start_calibration(double actual_distance);
 
