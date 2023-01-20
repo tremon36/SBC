c012044950f3fdf2771b05aa64c888f61f576b2b
Richi <tremon36xd@gmail.com>
2023-01-20 09:50:56 +0100

    final version

--- time_reference.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TIMER_REFERENCEH
-#define _TIMER_REFERENCEH
-//initialice time reference. Should be called after executing other functions;
-void time_reference_init(void);
-uint64_t time_reference_get_current_time_us(void);
-#endif
